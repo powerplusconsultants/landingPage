82c9ed3b8ca808dce23b961f2cb1baa7b853f7fa
Yosia Amado Suwitono <ysuwitono@asuransiastra.com>
2024-05-17 20:09:29 +0700

    penambahan porto ciputra

--- src/Component/Portofolio.js
+++ src/Component/Portofolio.js
@@ -7,6 +7,7 @@ import MiningImage from "./../assets/mining.jpg";
 import WhsImage from "./../assets/dupak.png";
 import Rodalink from "./../assets/rodalink.png";
 import Meratus from "./../assets/meratus.png";
+import Ciputra from "./../assets/ciputra2.png";
 
 const Portofolio = () => {
     return (
@@ -24,24 +25,29 @@ const Portofolio = () => {
                     <CardPorto src={SMCImage} title="Workshop Platinum Keramik" text={`1st floor, 148.000m2 <br /> PT. Surya Multi Cemerlang, Workshop Platinum Ceramic, Semambung – Sidoarjo`}/>
                 </div>
                 <div className="col-lg-3 col-md-6">
-                    <CardPorto src={Rodalink} title="RodaLink Showroom" text={`Bandung (3rd Storey, 400m2@fl <br /> Surabaya (4th Storey, 500m2@fl)`}/>
+                    <CardPorto src={Rodalink} title="RodaLink Showroom" text={`Bandung (3rd Storey, 400m2@fl) <br /> Surabaya (4th Storey, 500m2@fl)`}/>
                 </div>
                 <div className="col-lg-3 col-md-6">
-                    <CardPorto src={UBMImage} title="Universitas Bunda Mulia" text={"Universitas Bunda Mulia Housing – Ancol, Jakarta (12th Storey)"}/>
+                    <CardPorto src={UBMImage} title="Universitas Bunda Mulia" text={"Universitas Bunda Mulia Housing <br /> Ancol, Jakarta (12th Storey)"}/>
                 </div>
             </div>
             <div className="row mt-3 justify-content-center">
                 <div className="col-lg-3 col-md-6">
-                    <CardPorto src={ChruchImage} title="Gereja Graha Victory" text={"Gereja Graha Victory – Surabaya (7th Storey)"}/>
+                    <CardPorto src={ChruchImage} title="Gereja Graha Victory" text={"Gereja Graha Victory <br /> Surabaya (7th Storey)"}/>
                 </div>
                 <div className="col-lg-3 col-md-6">
                     <CardPorto src={MiningImage} title="Tambang Pasir Silika" text={`PT. Karya Emas Multisani <br /> Tambang Pasir Silika – East Belitung`}/>
                 </div>
                 <div className="col-lg-3 col-md-6">
-                    <CardPorto src={WhsImage} title="Toko dan Gudang" text={`Toko dan Gudang Dupak – Surabaya (5th Storey)`}/>
+                    <CardPorto src={WhsImage} title="Toko dan Gudang" text={`Toko dan Gudang Dupak <br /> Surabaya (5th Storey)`}/>
                 </div>
                 <div className="col-lg-3 col-md-6">
                     <CardPorto src={Meratus} title="Gudang dan Depot Container" text={`PT. Meratus Line <br /> Project warehouse and Depot Container at Cakung - Jakarta`}/>
+                </div>
+            </div>
+            <div className="row mt-3 justify-content-center">
+                <div className="col-lg-3 col-md-6">
+                    <CardPorto src={Ciputra} title="Ciputra Sport Center" text={"Citraland Surabaya <br /> Project Ciputra Sport Center"}/>
                 </div>
             </div>
         </div>
